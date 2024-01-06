f8ff12b17461bcc4f59e95739ac64cf197667bd6
Matias Wallenius <MatiWall@live.dk>
2024-01-06 15:24:22 +0100

    bug fix, naming

--- extensions/configuration/__init__.py
+++ extensions/configuration/__init__.py
@@ -1,1 +1,10 @@
-from .service import set_defaults, service_namespace, service_name, service_version, hostname, process_id, instance, environment
+from .service import (
+    set_defaults,
+    service_namespace,
+    service_name,
+    service_version,
+    hostname,
+    process_id,
+    service_instance,
+    environment
+)
