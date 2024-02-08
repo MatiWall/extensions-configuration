2d8e6cfe7c2cfbc69c25163325926f5c209dc405
Matias Wallenius <MatiWall@live.dk>
2024-02-08 20:49:22 +0100

    added catcode.yaml

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
  - DEPLOYABLE_UNIT
  - SERVICE_VERSION
  - ENVIRONMENT (Production, Development)
+
+# Bugs
+ - Does not create nested data classes, only first layer
--- extensions/configuration/config.py
+++ extensions/configuration/config.py
@@ -6,44 +6,38 @@ import yaml
 
 FILE_NAMES = [
     'secrets.yaml', 'secrets.yaml',
+    'appconfig.Production.yml', 'appconfig.Production.yaml',
     'appconfig.yml', 'appconfig.yaml',
-    'appconfig.Production.yml', 'appconfig.Production.yaml'
 ]
 
 def read_configs_to_dataclass(path: Path = Path.cwd()):
-    configs = read_configs(path)
-    appconfig = make_dataclass_from_config(configs)
+    config = read_configs(path)
+    appconfig = make_dataclass_from_config(config)
     return appconfig
 def read_configs(
         path: Path = Path.cwd(),
 ):
-    config_data = []
+    config_data = {}
 
     for file_name in FILE_NAMES:
         file_path = path / file_name
 
         if file_path.is_file():
             with open(file_path, 'r') as file:
-                config_data.append(yaml.safe_load(file))
+                config_data.update(yaml.safe_load(file))
 
     return config_data
 
 def make_dataclass_from_config(config: Union[list, tuple, dict], class_name: str = 'AppConfig'):
     fields_list = []
 
-    if isinstance(config, (dict,)):
-        config = [config]
 
-    _config = {}
-    for c in config:
-        _config.update(c)
-
-    for key, value in _config.items():
+    for key, value in config.items():
         if isinstance(value, dict):
             nested_class_name = key.capitalize() + "Data"
             nested_dataclass = make_dataclass_from_config(value, nested_class_name)
-            fields_list.append((key, Union[nested_dataclass, Any]))
+            fields_list.append((key, Union[Any]))
         else:
             fields_list.append((key, Any))
 
-    return make_dataclass(class_name, fields_list)
+    return make_dataclass(class_name, fields_list)(**config)
